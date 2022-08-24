30991c3b3b15070328f8c5d14f7ea3dd0d94d69c
“couture” <couture@outlook.com>
2022-08-24 22:27:01 +0800

    add:文件下载

--- src/main/java/com/couture/controller/CommonController.java
+++ src/main/java/com/couture/controller/CommonController.java
@@ -2,13 +2,15 @@ package com.couture.controller;
 
 import com.couture.common.R;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.UUID;
 
 /**
@@ -48,5 +50,38 @@ public class CommonController {
             e.printStackTrace();
         }
         return R.success(fileName);
+    }
+
+    /**
+     * 文件下载
+     *
+     * @param name 文件下载的名称
+     * @param response 响应
+     */
+    @GetMapping("/download")
+    public void downLoad(String name, HttpServletResponse response) {
+        try {
+            // 输入流，通过输入流读取文件内容
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath + name));
+            // 输出流，通过输出流将文件写会浏览器，在浏览器中显示图片了
+            ServletOutputStream outputStream = response.getOutputStream();
+
+            response.setContentType("image/jpeg");
+
+            int len = 0;
+            byte[] bytes = new byte[1024];
+            while ((len = fileInputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, len);
+                outputStream.flush();
+            }
+
+            // 关闭资源
+            outputStream.close();
+            fileInputStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
